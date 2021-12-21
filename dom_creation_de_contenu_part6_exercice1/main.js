fe30c76976b44db3355e02c6d10d5b83c507de9c
KlaNyange <nyangealixelambrette@gmail.com>
2021-12-22 00:54:52 +0100

    first commit

---
+++ dom_creation_de_contenu_part6_exercice1/main.js
@@ -0,0 +1,59 @@
+//*Exo1
+
+let div = document.getElementById("content")
+let h2 = document.createElement("h2")
+h2.innerText = "Part 6 - Exercice 1"
+div.append(h2)
+
+//*Exo2
+let p = document.createElement("p")
+p.innerText = "lorem ipsum"
+div.append(p)
+
+//*Exo3
+let h1 = document.createElement("h1")
+h1.innerText = "Le DOM - Création de HTML"
+div.prepend(h1)
+
+//*Exo4
+let div2 = document.getElementById("secondaire")
+// let contenu = div.innerHTML
+// div2.innerHTML = contenu
+div2.innerHTML = div.innerHTML
+div2.children[1].innerText = " Part 6 - Exercice 2"
+//done les essais d'une pauvre fille qui ne connait pas la définition du mot "contenu"
+
+//done let contenuh2 = h2.cloneNode(true)
+//done let contenup = p.cloneNode(true)
+//done div2.append(contenuh2, contenup)
+//done contenuh2.innerText = " Part 6 - Exercice 2"
+//*Exo5
+
+let div3 = document.createElement("div")
+let ol = document.createElement("ol")
+
+//METHODE 1
+// let li = document.createElement("li")
+// let li2 = document.createElement("li")
+// let li3 = document.createElement("li")
+// ol.children[0].innerText = " On peut créer des éléments"
+// ol.children[1].innerText = " On peut injecter dans ces éléments un autre élement"
+// ol.children[2].innerText = " On peut cloner des éléments"
+for (let i = 0; i < 3; i++) {
+    ol.append(document.createElement("li"))
+    if (ol.children[0]) {
+        ol.children[0].innerText = " On peut créer des éléments"
+    } if (ol.children[1]) {
+        ol.children[1].innerText = " On peut injecter dans ces éléments un autre élement"
+    } if (ol.children[2]) {
+        ol.children[2].innerText = " On peut cloner des éléments"
+    }
+}
+
+div3.id = "matiere"
+document.body.append(div3)
+div3.append(ol)
+
+
+
+
---
+++ dom_creation_de_contenu_part6_exercice2/main.js
@@ -0,0 +1,52 @@
+//*Exo1
+let delfirst = document.querySelector("input")
+let ul = document.querySelector("ul")
+
+delfirst.addEventListener("click", function(){
+    ul.removeChild(ul.firstElementChild)
+})
+
+//*Exo2
+let dellast = delfirst.nextElementSibling
+dellast.addEventListener("click", function(){
+    ul.removeChild(ul.lastElementChild)
+})
+
+//*Exo3
+let delrand = dellast.nextElementSibling
+delrand.addEventListener("click", function(){
+    if (ul.children.length > 0) {
+        ul.removeChild(ul.children[Math.floor(Math.random()*ul.children.length)])
+    }else {
+        console.warn("IL N'Y A PLUS RIEN A SUPPRIMER");
+    }
+})
+
+//*Exo4
+let ul2 = document.querySelectorAll("ul")[1]
+let li = ul2.children
+let newli = document.createElement("li")
+newli.innerText = "보라해"
+ul2.append(newli)
+let btn = document.querySelectorAll("input")[3]
+btn.addEventListener("click", function(){
+    ul2.replaceChild(ul2.lastElementChild, li[1])
+})
+
+//*Exo5
+let txt = document.querySelector("#replaceValue")
+let btn2 = document.querySelectorAll("input")[5]
+
+function replace() {
+    let newbi = document.createElement("li")
+    newbi.innerText = txt.value
+    ul2.append(newbi)
+    ul2.replaceChild(ul2.lastElementChild, li[2])
+}
+// function replace() {
+//     ul2.children[2].innerText = txt.value
+// }
+
+btn2.addEventListener("click", replace)
+
+
